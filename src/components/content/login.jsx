a5691156682d387565e36b86a05c02e3c46c354d
Martyna <martyna.sowinska@hotmail.com>
2022-02-09 22:32:25 +0100

    Updates on Login and Signup layout

--- src/components/content/login.jsx
+++ src/components/content/login.jsx
@@ -8,30 +8,40 @@ function Login() {
     return (
         <Route path={path.login}>
             <Container style={{ padding: "5rem 10rem" }}>
-
                 <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                    </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group className="submit-btn">
-                        <Button type="submit" className="btn-color">Log In</Button>
-                    </Form.Group>
+                    <Col xs={5} style={{ margin: "auto" }}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+                    </Col>
+
+                    <Col xs={5} style={{ margin: "auto" }}>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                    </Col>
+
+                    <Col xs={5} style={{ margin: "auto" }}>
+                        <Form.Group className="submit-btn">
+                            <Button type="submit" className="btn-color">Log In</Button>
+                        </Form.Group>
+                    </Col>
 
                     <br />
                     <br />
                     <br />
-                    <Row className="justify-content-md-center">
-                        <Col xs lg="4" style={{ textAlign: 'end' }}>Don't have an account?</Col>
-                        <Col xs lg="3" className="p-0"><Link to="/signup" className="links">Sign Up here!</Link></Col>
-                    </Row>
+
+                    <Col xs={5} style={{ margin: "auto" }}>
+                        <Row className="justify-content-md-center" >
+                            <Col xs lg="8" >Don't have an account?</Col>
+                            <Col xs lg="4" style={{ paddingLeft: '0' }}><Link to="/signup" className="links">Sign Up here!</Link></Col>
+                        </Row>
+                    </Col>
+
                 </Form>
-
             </Container>
         </Route>
     );
--- src/components/content/signup.jsx
+++ src/components/content/signup.jsx
@@ -8,35 +8,47 @@ function Signup() {
     return (
         <Route path={path.signup}>
             <Container style={{ padding: "5rem 10rem" }}>
-
                 <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                    </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
+                    <Col xs={5} style={{ margin: "auto" }}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+                    </Col>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Repeat password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
+                    <Col xs={5} style={{ margin: "auto" }}>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                    </Col>
 
-                    <Form.Group className="submit-btn">
-                        <Button type="submit" className="btn-color">Sign Up</Button>
-                    </Form.Group>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <Row className="justify-content-md-center">
-                        <Col xs lg="4" style={{textAlign: 'end'}}>Already have an account?</Col>
-                        <Col xs lg="3" className="p-0"><Link to="/login" className="links">Log In here!</Link></Col>
-                    </Row>
+                    <Col xs={5} style={{ margin: "auto" }}>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Repeat password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                    </Col>
+
+                    <Col xs={5} style={{ margin: "auto" }}>
+                        <Form.Group className="submit-btn">
+                            <Button type="submit" className="btn-color">Sign Up</Button>
+                        </Form.Group>
+                    </Col>
+
+                    <br />
+                    <br />
+                    <br />
+
+                    <Col xs={5} style={{ margin: "auto" }}>
+                        <Row className="justify-content-md-center" >
+                            <Col xs lg="8" >Already have an account?</Col>
+                            <Col xs lg="4" style={{ paddingLeft: '0' }}><Link to="/login" className="links">Log In here!</Link></Col>
+                        </Row>
+                    </Col>
+
                 </Form>
-
             </Container>
         </Route>
     );
